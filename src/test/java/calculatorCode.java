eabac38efe2601f9924208c4b653e34ca19bd0bc
neha1412 <nehakum287@gmail.com>
2022-01-27 14:37:55 +0530

    Initial Commit

---
+++ src/test/java/calculatorCode.java
@@ -0,0 +1,13 @@
+public class calculatorCode {
+
+    public int getProduct(int number1, int number2) {
+        int product= number1 * number2;
+        return product;
+    }
+
+
+    public int getSum(int number1, int number2) {
+        int sum= number1 + number2;
+        return sum;
+    }
+}
---
+++ src/test/java/calculatorTests.java
@@ -0,0 +1,23 @@
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class calculatorTests {
+
+    @Test
+    public void toGetsum(){
+
+        int sum = new calculatorCode().getSum(10, 40);
+        Assert.assertEquals(sum, 50);
+    }
+
+    @Test
+    public void toGetProduct(){
+        int product = new calculatorCode().getProduct(2, 30);
+        Assert.assertEquals(product, 60);
+
+    }
+
+
+}
+
